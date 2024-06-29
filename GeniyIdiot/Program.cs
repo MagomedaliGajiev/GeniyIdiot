21df56bb81ff3cd63b5ccad967d2b90296097971
Magomedali Gajiev <mag198421@gmail.com>
2024-06-29 08:48:14 +0300

    Ввод вопросов и ответов и подсчет правильных ответов

--- GeniyIdiot/Program.cs
+++ GeniyIdiot/Program.cs
@@ -10,6 +10,30 @@
             questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
             questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
             questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+
+            var answers = new int[5];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+
+            var countRightAnswers = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(questions[i]);
+
+                var userAnswer = Convert.ToInt32(Console.ReadLine());
+
+                var rightAnswer = answers[i];
+
+                if (userAnswer == rightAnswer)
+                {
+                    ++countRightAnswers;
+                }
+            }
+            Console.WriteLine($"Количество правильных ответов: {countRightAnswers}");
         }
     }
 }
