668a301bfd29c6b53abffe6cbfdd1c5a3fa39a4d
Magomedali Gajiev <mag198421@gmail.com>
2024-06-29 16:53:16 +0300

    Убрал параметры метода GetDiagnoses

--- GeniyIdiot/Program.cs
+++ GeniyIdiot/Program.cs
@@ -2,42 +2,6 @@
 {
     class Program
     {
-        private static string[] GetQuestions(int countQuestions)
-        {
-            var questions = new string[countQuestions];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-
-            return questions;
-        }
-
-        private static int[] GetAnswers(int countAnswers)
-        {
-            var answers = new int[countAnswers];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
-
-            return answers;
-        }
-
-        private static string[] GetDiagnoses(int countDiagnoses)
-        {
-            var diagnoses = new string[countDiagnoses];
-            diagnoses[0] = "кретин";
-            diagnoses[1] = "идиот";
-            diagnoses[2] = "дурак";
-            diagnoses[3] = "нормальный";
-            diagnoses[4] = "талант";
-            diagnoses[5] = "гений";
-
-            return diagnoses;
-        }
         static void Main(string[] args)
         {
             var countQuestions = 5;
@@ -67,8 +31,44 @@
             }
             Console.WriteLine($"Количество правильных ответов: {countRightAnswers}");
 
-            var countDiagnoses = 6;
-            var diagnoses = GetDiagnoses(countDiagnoses);
+            var diagnoses = GetDiagnoses();
+        }
+
+        private static string[] GetQuestions(int countQuestions)
+        {
+            var questions = new string[countQuestions];
+            questions[0] = "Сколько будет два плюс два умноженное на два?";
+            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+
+            return questions;
+        }
+
+        private static int[] GetAnswers(int countAnswers)
+        {
+            var answers = new int[countAnswers];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+
+            return answers;
+        }
+
+        private static string[] GetDiagnoses()
+        {
+            var diagnoses = new string[countDiagnoses];
+            diagnoses[0] = "кретин";
+            diagnoses[1] = "идиот";
+            diagnoses[2] = "дурак";
+            diagnoses[3] = "нормальный";
+            diagnoses[4] = "талант";
+            diagnoses[5] = "гений";
+
+            return diagnoses;
         }
     }
 }
