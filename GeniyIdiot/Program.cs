5988040818e9ccc4cb85d6fcac865b031ec64787
Magomedali Gajiev <mag198421@gmail.com>
2024-06-29 16:35:17 +0300

    Внес определенные изменения

--- GeniyIdiot/Program.cs
+++ GeniyIdiot/Program.cs
@@ -2,32 +2,63 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string[] GetQuestions(int countQuestions)
         {
-            var questions = new string[5];
+            var questions = new string[countQuestions];
             questions[0] = "Сколько будет два плюс два умноженное на два?";
             questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
             questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
             questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
             questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
 
-            var answers = new int[5];
+            return questions;
+        }
+
+        private static int[] GetAnswers(int countAnswers)
+        {
+            var answers = new int[countAnswers];
             answers[0] = 6;
             answers[1] = 9;
             answers[2] = 25;
             answers[3] = 60;
             answers[4] = 2;
 
+            return answers;
+        }
+
+        private static string[] GetDiagnoses(int countDiagnoses)
+        {
+            var diagnoses = new string[countDiagnoses];
+            diagnoses[0] = "кретин";
+            diagnoses[1] = "идиот";
+            diagnoses[2] = "дурак";
+            diagnoses[3] = "нормальный";
+            diagnoses[4] = "талант";
+            diagnoses[5] = "гений";
+
+            return diagnoses;
+        }
+        static void Main(string[] args)
+        {
+            var countQuestions = 5;
+            var questions = GetQuestions(countQuestions);
+
+            var answers = GetAnswers(countQuestions);
+
             var countRightAnswers = 0;
 
-            for (int i = 0; i < 5; i++)
+            var random = new Random();
+
+            for (int i = 0; i < countQuestions; i++)
             {
                 Console.WriteLine($"Вопрос №{i + 1}");
-                Console.WriteLine(questions[i]);
+
+                var randomQuestionIndex = random.Next(0, countQuestions);
+                Console.WriteLine(questions[randomQuestionIndex]);
 
                 var userAnswer = Convert.ToInt32(Console.ReadLine());
 
-                var rightAnswer = answers[i];
+                var rightAnswer = answers[randomQuestionIndex];
 
                 if (userAnswer == rightAnswer)
                 {
@@ -36,14 +67,8 @@
             }
             Console.WriteLine($"Количество правильных ответов: {countRightAnswers}");
 
-            var diagnoses = new string[6];
-            diagnoses[0] = "кретин";
-            diagnoses[1] = "идиот";
-            diagnoses[2] = "дурак";
-            diagnoses[3] = "нормальный";
-            diagnoses[4] = "талант";
-            diagnoses[5] = "гений";
-            Console.WriteLine($"Ваш диагноз:{diagnoses[countRightAnswers]}");
+            var countDiagnoses = 6;
+            var diagnoses = GetDiagnoses(countDiagnoses);
         }
     }
 }
