9286c7789b169606fcb419415e5836156c1b2b5d
Magomedali Gajiev <mag198421@gmail.com>
2024-06-30 16:14:50 +0300

    Добавлен класс FileProvider для работы с файловой системой

---
+++ GeniyIdiot/FileProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace GeniyIdiot
+{
+    public class FileProvider
+    {
+        public static void Append(string fileName, string value)
+        {
+            var writer = new StreamWriter(fileName, true, Encoding.UTF8);
+            writer.WriteLine(value);
+            writer.Close();
+        }
+
+        public static string GetValue(string fileName)
+        {
+            var reader = new StreamReader(fileName, Encoding.UTF8);
+            var value = reader.ReadToEnd();
+            reader.Close();
+            return value;
+        }
+    }
+}
--- GeniyIdiot/UserResultsStorage.cs
+++ GeniyIdiot/UserResultsStorage.cs
@@ -1,29 +1,19 @@
-﻿using System.Text;
-
-namespace GeniyIdiot
+﻿namespace GeniyIdiot
 {
     public class UserResultsStorage
     {
         public static void Save(User user)
         {
             var value = $"{user.Name}#{user.CountRightAnswers}#{user.Diagnosis}";
-            AppendToFile("userResults.txt", value);
+            FileProvider.Append("userResults.txt", value);
         }
-        public static void AppendToFile(string fileName, string value)
-        {
-            var writer = new StreamWriter(fileName, true, Encoding.UTF8);
-            writer.WriteLine(value);
-            writer.Close();
-        }
-
         public static List<User> GetUserResults()
         {
+            var value = FileProvider.GetValue("userResults.txt");
+            var lines = value.Split('\n');
             var results = new List<User>();
-
-            var reader = new StreamReader("userResults.txt", Encoding.UTF8);
-            while (!reader.EndOfStream)
+            foreach (var line in lines)
             {
-                var line = reader.ReadLine();
                 var values = line.Split('#');
                 var name = values[0];
                 var countRightAnswers = Convert.ToInt32(values[1]);
@@ -35,8 +25,6 @@ namespace GeniyIdiot
 
                 results.Add(user);
             }
-            reader.Close();
-
             return results;
         }
     }
