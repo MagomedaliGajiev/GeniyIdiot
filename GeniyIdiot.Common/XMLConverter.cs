a8bc7948fd4372d346e5c7f855614dd3df2c4a5e
Magomedali Gajiev <mag198421@gmail.com>
2024-07-03 22:43:15 +0300

    Добавил XMLConverter

---
+++ GeniyIdiot.Common/XMLConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Serialization;
+
+namespace GeniyIdiot.Common
+{
+    public class XMLConverter : IConvert
+    {
+        public string Serialize<T>(T item)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, item);
+                return textWriter.ToString();
+            }
+        }
+        public T Deserialize<T>(string xml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var textReader = new StringReader(xml))
+            {
+                return (T)xmlSerializer.Deserialize(textReader);
+            }
+        }
+    }
+}
